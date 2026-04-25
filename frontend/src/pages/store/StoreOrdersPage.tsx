d40b6b5a7fc273eab6f9d24226855deca9ee5e98
mariacastro11 <maria1.castro.stj@gmail.com>
2026-04-24 19:31:57 -0500

    Fix: put enviroment variable backend URL

--- frontend/src/pages/store/StoreOrdersPage.tsx
+++ frontend/src/pages/store/StoreOrdersPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { getStoreOrders } from "../../services/order.service";
 import { supabase } from "../../services/supabase";
 
